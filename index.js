cada207563c144f4ccfc31670f395e1364b7d5dc
thmshreyas <125906781+thmshreyas@users.noreply.github.com>
2024-01-04 18:55:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+document.querySelectorAll(".drum")[i].addEventListener("click",function()
+{
+ var but=this.innerHTML;
+ makesound(but);
+ buttonanimation(but);
+
+})
+document.addEventListener("keydown",function(event){
+    makesound(event.key);
+    buttonanimation(event.key);
+});
+function makesound(key){
+    switch(key){
+        case "w":
+            var   audio=new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a":
+            var   audio=new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+            var   audio=new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "d":
+            var   audio=new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case "j":
+            var   audio=new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+        case "k":
+            var   audio=new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+        case "l":
+            var   audio=new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        
+    
+     }
+
+}
+function buttonanimation(currentkey){
+    var activeb= document.querySelector("."+currentkey);
+    activeb.classList.add("pressed");
+    setTimeout(function(){activeb.classList.remove("pressed");},100);
+
+}
